fb48ff1bbb92fefcfb8edd5bb9e0415ce0d9da3f
K² Technologies <81196396+K2Technologies@users.noreply.github.com>
2022-11-29 13:33:20 -0700

    Update DeleteBadRegKeys.ps1

--- DeleteBadRegKeys.ps1
+++ DeleteBadRegKeys.ps1
@@ -1,25 +1,19 @@
-﻿$PatternSID = 'S-1-5-21-\d+-\d+\-\d+\-\d+$'
+$PatternSID = 'S-1-5-21-\d+-\d+\-\d+\-\d+$'
 # Get Username, SID, and location of ntuser.dat for all users
 $ProfileList = @()
-  $ProfileList = Get-ItemProperty 'HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\ProfileList\*' | Where-Object { $_.PSChildName ‑Match $PatternSID } |
-    Select  @{ name = "SID"; expression = { $_.PSChildName } },
+  $ProfileList = "Get-ItemProperty 'HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\ProfileList\*' | Where-Object { $_.PSChildName ‑match $PatternSID } |
+    Select  @{ name = "SID"; expression = { $_.PSChildName } }",
      @{ name = "UserHive"; expression = { "$($_.ProfileImagePath)\ntuser.dat" } },
      @{ name = "Username"; expression = { $_.ProfileImagePath -replace '^(.*[\\\/])', '' } 
         }
 
 
-# Get all user SIDs found in HKEY_USERS (ntuser.dat files that are loaded)
-$LoadedHives = Get-ChildItem Registry::HKEY_USERS | ? { $_.PSChildname ‑match $PatternSID } | Select @{ name = "SID"; expression = { $_.PSChildName } }
 
-$SIDObject = @()  
-  foreach ($item in $LoadedHives)
-  {
-      $props = @{
-          SID = $item.SID
-      }
-      $TempSIDObject = New-Object -TypeName PSCustomObject -Property $props
-      $SIDObject += $TempSIDObject
-  }
+
+# Get all user SIDs found in HKEY_USERS (ntuser.dat files that are loaded)
+$LoadedHives = "Get-ChildItem Registry::HKEY_USERS | ? { $_.PSChildname ‑match $PatternSID } | Select @{ name = "SID"; expression = { $_.PSChildName } }"
+
+
 
 # We need to use ($ProfileList | Measure-Object).count instead of just ($ProfileList).count
 # because in PS V2, if the count is less than 2, it doesn't work. :)
