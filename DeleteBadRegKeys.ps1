042988e28793f4bcc841993b7ccca4777d8e1c66
K² Technologies <81196396+K2Technologies@users.noreply.github.com>
2022-11-29 13:44:41 -0700

    Update DeleteBadRegKeys.ps1

--- DeleteBadRegKeys.ps1
+++ DeleteBadRegKeys.ps1
@@ -1,43 +1,35 @@
+# Regex pattern for SIDs
 $PatternSID = 'S-1-5-21-\d+-\d+\-\d+\-\d+$'
+ 
 # Get Username, SID, and location of ntuser.dat for all users
-$ProfileList = @()
-  $ProfileList = "Get-ItemProperty 'HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\ProfileList\*' | Where-Object { $_.PSChildName ‑match $PatternSID } |
-    Select  @{ name = 'SID'; expression = { $_.PSChildName } }",
-     @{ name = "UserHive"; expression = { "$($_.ProfileImagePath)\ntuser.dat" } },
-     @{ name = "Username"; expression = { $_.ProfileImagePath -replace '^(.*[\\\/])', '' } 
-        }
+$ProfileList = gp 'HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\ProfileList\*' | Where-Object {$_.PSChildName -match $PatternSID} | 
+    Select  @{name="SID";expression={$_.PSChildName}}, 
+            @{name="UserHive";expression={"$($_.ProfileImagePath)\ntuser.dat"}}, 
+            @{name="Username";expression={$_.ProfileImagePath -replace '^(.*[\\\/])', ''}}
+ 
+# Get all user SIDs found in HKEY_USERS (ntuder.dat files that are loaded)
+$LoadedHives = gci Registry::HKEY_USERS | ? {$_.PSChildname -match $PatternSID} | Select @{name="SID";expression={$_.PSChildName}}
+ 
+# Get all users that are not currently logged
+$UnloadedHives = Compare-Object $ProfileList.SID $LoadedHives.SID | Select @{name="SID";expression={$_.InputObject}}, UserHive, Username
+ 
+# Loop through each profile on the machine
+Foreach ($item in $ProfileList) {
+    # Load User ntuser.dat if it's not already loaded
+    IF ($item.SID -in $UnloadedHives.SID) {
+        reg load HKU\$($Item.SID) $($Item.UserHive) | Out-Null
+    }
+ 
+    #####################################################################
+    # This is where you can read/modify a users portion of the registry 
+        reg del HKCU\SOFTWARE\Infosoft\SoftDent
 
-
-
-
-# Get all user SIDs found in HKEY_USERS (ntuser.dat files that are loaded)
-$LoadedHives = "Get-ChildItem Registry::HKEY_USERS | ? { $_.PSChildname ‑match $PatternSID } | Select @{ name = 'SID'; expression = { $_.PSChildName } }"
-
-
-
-# We need to use ($ProfileList | Measure-Object).count instead of just ($ProfileList).count
-# because in PS V2, if the count is less than 2, it doesn't work. :)
-for ($p = 0; $p -lt ($ProfileList | Measure-Object).count; $p++)
-{
-  for ($l = 0; $l -lt ($SIDObject | Measure-Object).count; $l++)
-  {
-      if (($ProfileList[$p].SID) -ne ($SIDObject[$l].SID))
-      {
-          $UnloadedHives += $ProfileList[$p].SID
-          Write-Verbose -Message "Loading Registry hives for $($ProfileList[$p].SID)"
-          reg load "HKU\$($ProfileList[$p].SID)" "$($ProfileList[$p].UserHive)"
-          Write-Verbose -Message 'Attempting to remove registry keys for each profile'
-          #####################################################################
-          # This is where you can read/modify a user's portion of the registry 
-          reg del HKCU\SOFTWARE\Infosoft\SoftDent
-      }
-  }
+#####################################################################
+ 
+    # Unload ntuser.dat        
+    IF ($item.SID -in $UnloadedHives.SID) {
+        ### Garbage collection and closing of ntuser.dat ###
+        [gc]::Collect()
+        reg unload HKU\$($Item.SID) | Out-Null
+    }
 }
-
-
-
-Write-Verbose 'Unloading registry hives for all users'
-# Unload ntuser.dat        
-# Garbage collection and closing of ntuser.dat ###
-[gc]::Collect()
-reg unload "HKU\$($ProfileList[$p].SID)"
